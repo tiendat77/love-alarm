33902f7fb3639f1e251217d3b3e8660840d8154d
Ji Woo <huynhztienzdat@gmail.com>
2022-03-21 23:20:35 +0700

    update: modal my-qr-code

--- src/app/modals/my-qr-code/my-qr-code.component.ts
+++ src/app/modals/my-qr-code/my-qr-code.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import QRCode from 'qrcode';
+
+import { UserService } from '../../services';
 
 @Component({
   selector: 'app-my-qr-code',
@@ -8,9 +11,30 @@ import { ModalController } from '@ionic/angular';
 })
 export class MyQrCodeModal {
 
+  qrcode: string;
+
   constructor(
-    private modalCtrl: ModalController
-  ) {}
+    private modalCtrl: ModalController,
+
+    public readonly user: UserService,
+  ) {
+    this.init();
+  }
+
+  private init() {
+    const code = 'lovealarm://' + this.user.id;
+    QRCode.toDataURL(code, {
+      type: 'image/webp',
+      width: 300,
+      margin: 0,
+    })
+    .then(url => {
+      this.qrcode = url;
+    })
+    .catch(err => {
+      console.error(err);
+    });
+  }
 
   close() {
     this.modalCtrl.dismiss();
