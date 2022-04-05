82cb4d040af301e4190967c106b5d087504c8d04
Ji Woo <huynhztienzdat@gmail.com>
2022-04-06 00:10:12 +0700

    ✨  add: tutorial page

--- src/app/pages/tutorial/tutorial.page.ts
+++ src/app/pages/tutorial/tutorial.page.ts
@@ -1,5 +1,9 @@
-import { Component, NgZone, ViewChild } from '@angular/core';
-import { SwiperComponent } from 'swiper/angular';
+import { ChangeDetectorRef, Component, NgZone } from '@angular/core';
+import { Router } from '@angular/router';
+import { Storage } from '@ionic/storage';
+import { Swiper } from 'swiper';
+
+import { STORAGE_KEY } from '../../configs/storage-key';
 
 @Component({
   selector: 'app-tutorial',
@@ -8,17 +12,81 @@ import { SwiperComponent } from 'swiper/angular';
 })
 export class TutorialPage {
 
-  @ViewChild('swiper', { static: false }) swiper?: SwiperComponent;
-  activeTab = 0;
+  swiper: Swiper;
+  showSkip = true;
+
+  step = 1;
+  percent = 0;
+  offset = null;
+
+  readonly total = 3;
+  readonly meter = (19.5 * 2) * Math.PI; // radius * 2 * PI
 
   constructor(
-    private ngZone: NgZone
-  ) { }
+    private router: Router,
+    private storage: Storage,
+    private ngZone: NgZone,
+    private cd: ChangeDetectorRef,
+  ) {}
+
+  ngOnInit() {
+    this.updateIndicator();
+  }
+
+  ionViewWillEnter() {
+    this.storage.get(STORAGE_KEY.TUTORIAL).then(res => {
+      if (res === true) {
+        this.router.navigateByUrl(
+          '/app/home',
+          { replaceUrl: true }
+        );
+      }
+    });
+  }
+
+  next() {
+    if (this.step < this.total) {
+      this.step++;
+      this.updateIndicator();
+      this.swiper.slideNext();
+      return;
+    }
+
+    this.startApp();
+  }
+
+  private updateIndicator() {
+    this.percent = this.step / this.total * 100;
+    this.offset = this.meter - (this.meter * (this.clamp(this.percent) / 100));
+  }
+
+  private clamp(value: number, min = 0, max = 100) {
+    return Math.min(Math.max(value, min), max);
+  }
+
+  startApp() {
+    this.router
+    .navigateByUrl('/app/home', { replaceUrl: true })
+    .then(() => {
+      // TODO
+      // this.storage.set(STORAGE_KEY.TUTORIAL, true);
+    });
+  }
+
+  setSwiperInstance(swiper: Swiper) {
+    this.swiper = swiper;
+  }
 
   onSlideChange([swiper]) {
     this.ngZone.run(() => {
-      this.activeTab = swiper.realIndex;
+      this.step = swiper.realIndex + 1;
+      this.updateIndicator();
     });
+  }
+
+  onSlideChangeStart() {
+    this.showSkip = !this.swiper.isEnd;
+    this.cd.detectChanges();
   }
 
 }
